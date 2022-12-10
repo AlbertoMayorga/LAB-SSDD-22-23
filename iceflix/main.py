d2b26790e3a1eae9ec3d2dc875e06db54cd38afd
AlbertoMayorga <Alberto.Mayorga@alu.uclm.es>
2022-12-10 19:09:32 +0100

    Changes in main

--- iceflix/main.py
+++ iceflix/main.py
@@ -3,6 +3,8 @@
 import logging
 import sys
 import uuid
+import threading
+import random
 
 import Ice
 Ice.loadSlice('iceflix.ice')
@@ -19,6 +21,9 @@ class Main(IceFlix.Main):
         self.catalogList = []
         self.fileList = []
         self.mains = {}
+        self._id_ = str(uuid.uuid4())
+        self.timer = timer 
+        self.updated = False
 
     def getAuthenticator(self, current=None):  # pylint:disable=invalid-name, unused-argument
         "Return the stored Authenticator proxy."
@@ -31,7 +36,6 @@ class Main(IceFlix.Main):
                 except Ice.ConnectionRefusedException as e:
                     print(e)
         raise IceFlix.TemporaryUnavaible()
-
 
     def getCatalog(self, current=None):  # pylint:disable=invalid-name, unused-argument
         "Return the stored MediaCatalog proxy."
@@ -59,54 +63,96 @@ class Main(IceFlix.Main):
 
     def newService(self, proxy, service_id, current=None):  # pylint:disable=invalid-name, unused-argument
         "Receive a proxy of a new service."
-
-        return
-
+        if service_id != str(uuid.uuid4()):
+            if proxy.ice_isA('::IceFlix::Main'):
+                proxyService = IceFlix.MainPrx.checkedCast(proxy)
+                if str(uuid.uuid4()) != self._id_ and self.updated is False:
+                    self.timer.cancel()
+                    proxyService.authenticatorList = self.authenticatorList
+                    proxyService.catalogList = self.catalogList
+                    proxyService.fileList = self.fileList
+                    self.updated = True
+            if proxy.ice_isA('::IceFlix::Authenticator'):
+                proxyService = IceFlix.AuthenticatorPrx.checkedCast(proxy)
+                if str(uuid.uuid4()) != self._id_ and self.updated is False:
+                    self.timer.cancel()
+                    proxyService.authenticatorList = self.authenticatorList
+                    proxyService.catalogList = self.catalogList
+                    proxyService.fileList = self.fileList
+                    self.updated = True
+            if proxy.ice_isA('::IceFlix::MediaCatalog'):
+                proxyService = IceFlix.MediaCatalogPrx.checkedCast(proxy)
+                if str(uuid.uuid4()) != self._id_ and self.updated is False:
+                    self.timer.cancel()
+                    proxyService.authenticatorList = self.authenticatorList
+                    proxyService.catalogList = self.catalogList
+                    proxyService.fileList = self.fileList
+                    self.updated = True
+            if proxy.ice_isA('::IceFlix::FileService'):
+                proxyService = IceFlix.FileServicePrx.checkedCast(proxy)
+                if str(uuid.uuid4()) != self._id_ and self.updated is False:
+                    self.timer.cancel()
+                    proxyService.authenticatorList = self.authenticatorList
+                    proxyService.catalogList = self.catalogList
+                    proxyService.fileList = self.fileList
+                    self.updated = True
 
     def announce(self, proxy, service_id, current=None):  # pylint:disable=invalid-name, unused-argument
         "Announcements handler."
-        if proxy in (list(authenticatorList) + list(catalogList) + list(fileList) + list(mains.values())) or service_id == str(uuid.uuid4()):
+        if proxy in (list(self.authenticatorList) + list(self.catalogList) + list(self.fileList) + list(self.mains.values())) or service_id == str(uuid.uuid4()):
             return
 
         if proxy.ice_isA('::IceFlix::Authenticator'):
-            print(f'New Authenticator service: {service_id}')
+            print(f'New AuthServer: {service_id}')
             self.authenticatorList.append(IceFlix.AuthenticatorPrx.uncheckedCast(proxy))
 
         if proxy.ice_isA('::IceFlix::MediaCatalog'):
-            print(f'New Catalog service: {service_id}')
+            print(f'New CatalogServer: {service_id}')
             self.catalogList.append(IceFlix.MediaCatalogPrx.uncheckedCast(proxy))
 
         if proxy.ice_isA('::IceFlix::FileService'):
-            print(f'New File service: {service_id}')
-            self.fileList.append(IceFlix.FileService.uncheckedCast(proxy))
+            print(f'New FileServer: {service_id}')
+            self.fileList.append(IceFlix.FileServicePrx.uncheckedCast(proxy))
 
         if proxy.ice_isA('::IceFlix::Main'):
-            print(f'New Main service: {service_id}')
+            print(f'New MainServer: {service_id}')
             self.mains.update({service_id:proxy})
 
 class MainApp(Ice.Application):
     """Example Ice.Application for a Main service."""
-
     def __init__(self):
         super().__init__()
-        self.servant = Main()
+        self.timerr = threading.Timer(3, print("First Main!!"))
+        self.servant = Main(self.timerr)
         self.proxy = None
         self.adapter = None
 
-    def initial(self, current=None):
-        print("First! Main")
+    def annoucement(self, servant, proxy):
+        servant.announce(proxy, str(uuid.uuid4()))
+        timer = threading.Timer((10+random.randint(-2,2)), self.annoucement, (servant, proxy))
+        timer.start()
 
     def run(self, args):
         """Run the application, adding the needed objects to the adapter."""
         logging.info("Running Main application")
         comm = self.communicator()
         self.adapter = comm.createObjectAdapter("MainAdapter")
+        self.proxy = self.adapter.addWithUUID(self.servant)
         self.adapter.activate()
 
-        self.proxy = self.adapter.addWithUUID(self.servant)
+        self.servant.newService(self.proxy, str(uuid.uuid4()))
+
+        self.timerr.start()
+
+        timer = threading.Timer(10, self.annoucement, (self.servant, self.proxy))
+        timer.start()
+
+        print("Main Server: " +str(self.proxy), flush=True)
 
         self.shutdownOnInterrupt()
         comm.waitForShutdown()
+        timer.cancel()
+        self.timerr.cancel()
 
         return 0
 
