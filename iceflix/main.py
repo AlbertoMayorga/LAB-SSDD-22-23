47f7e54dca8172db909610bd78e1e98cc6f4e5e8
AlbertoMayorga <Alberto.Mayorga@alu.uclm.es>
2022-12-11 23:24:07 +0100

    ordinaria-p1

--- iceflix/main.py
+++ iceflix/main.py
@@ -4,10 +4,9 @@ import logging
 import sys
 import uuid
 import threading
-import random
 
 import Ice
-Ice.loadSlice('iceflix.ice')
+Ice.loadSlice('iceflix/iceflix.ice')
 import IceFlix  # pylint:disable=import-error
 
 class Main(IceFlix.Main):
@@ -17,106 +16,100 @@ class Main(IceFlix.Main):
     for this interface. Use it with caution
     """
     def __init__(self, timer):
-        self.authenticatorList = []
-        self.catalogList = []
-        self.fileList = []
+        self.authenticator_list = []
+        self.catalog_list = []
+        self.file_list = []
         self.mains = {}
         self._id_ = str(uuid.uuid4())
-        self.timer = timer 
+        self.timer = timer
         self.updated = False
 
     def getAuthenticator(self, current=None):  # pylint:disable=invalid-name, unused-argument
         "Return the stored Authenticator proxy."
-        if len(self.authenticatorList) != 0:
-            for i in self.authenticatorList:
+        if len(self.authenticator_list) != 0:
+            for i in self.authenticator_list:
                 print(i)
                 try:
                     i.ice_ping()
                     return IceFlix.AuthenticatorPrx.checkedCast(i)
-                except Ice.ConnectionRefusedException as e:
-                    print(e)
+                except Ice.ConnectionRefusedException as exception:
+                    print(exception)
         raise IceFlix.TemporaryUnavaible()
 
     def getCatalog(self, current=None):  # pylint:disable=invalid-name, unused-argument
         "Return the stored MediaCatalog proxy."
-        if len(self.catalogList) != 0:
-            for i in self.catalogList:
+        if len(self.catalog_list) != 0:
+            for i in self.catalog_list:
                 print(i)
                 try:
                     i.ice_ping()
                     return IceFlix.MediaCatalogPrx.checkedCast(i)
-                except Ice.ConnectionRefusedException as e:
-                    print(e)
+                except Ice.ConnectionRefusedException as exception:
+                    print(exception)
         raise IceFlix.TemporaryUnavaible()
 
     def getFileService(self, current=None): # pylint:disable=invalid-name, unused-argument
         "Return the stored FileService proxy."
-        if len(self.fileList) != 0:
-            for i in self.fileList:
+        if len(self.file_list) != 0:
+            for i in self.file_list:
                 print(i)
                 try:
                     i.ice_ping()
                     return IceFlix.FileServicePrx.checkedCast(i)
-                except Ice.ConnectionRefusedException as e:
-                    print(e)
+                except Ice.ConnectionRefusedException as exception:
+                    print(exception)
         raise IceFlix.TemporaryUnavaible()
 
     def newService(self, proxy, service_id, current=None):  # pylint:disable=invalid-name, unused-argument
         "Receive a proxy of a new service."
         if service_id != str(uuid.uuid4()):
             if proxy.ice_isA('::IceFlix::Main'):
-                proxyService = IceFlix.MainPrx.checkedCast(proxy)
+                proxy_service = IceFlix.MainPrx.checkedCast(proxy)
                 if str(uuid.uuid4()) != self._id_ and self.updated is False:
                     self.timer.cancel()
-                    proxyService.authenticatorList = self.authenticatorList
-                    proxyService.catalogList = self.catalogList
-                    proxyService.fileList = self.fileList
+                    proxy_service.authenticator_list = self.authenticator_list
+                    proxy_service.catalog_list = self.catalog_list
+                    proxy_service.file_list = self.file_list
                     self.updated = True
+
             if proxy.ice_isA('::IceFlix::Authenticator'):
-                proxyService = IceFlix.AuthenticatorPrx.checkedCast(proxy)
+                proxy_service = IceFlix.AuthenticatorPrx.checkedCast(proxy)
                 if str(uuid.uuid4()) != self._id_ and self.updated is False:
                     self.timer.cancel()
-                    proxyService.authenticatorList = self.authenticatorList
-                    proxyService.catalogList = self.catalogList
-                    proxyService.fileList = self.fileList
                     self.updated = True
+
             if proxy.ice_isA('::IceFlix::MediaCatalog'):
-                proxyService = IceFlix.MediaCatalogPrx.checkedCast(proxy)
+                proxy_service = IceFlix.MediaCatalogPrx.checkedCast(proxy)
                 if str(uuid.uuid4()) != self._id_ and self.updated is False:
                     self.timer.cancel()
-                    proxyService.authenticatorList = self.authenticatorList
-                    proxyService.catalogList = self.catalogList
-                    proxyService.fileList = self.fileList
                     self.updated = True
+
             if proxy.ice_isA('::IceFlix::FileService'):
-                proxyService = IceFlix.FileServicePrx.checkedCast(proxy)
+                proxy_service = IceFlix.FileServicePrx.checkedCast(proxy)
                 if str(uuid.uuid4()) != self._id_ and self.updated is False:
                     self.timer.cancel()
-                    proxyService.authenticatorList = self.authenticatorList
-                    proxyService.catalogList = self.catalogList
-                    proxyService.fileList = self.fileList
                     self.updated = True
 
     def announce(self, proxy, service_id, current=None):  # pylint:disable=invalid-name, unused-argument
         "Announcements handler."
-        if proxy in (list(self.authenticatorList) + list(self.catalogList) + list(self.fileList) + list(self.mains.values())) or service_id == str(uuid.uuid4()):
-            return
+        services = list(self.authenticator_list) + list(self.catalog_list) + list(self.file_list) + list(self.mains.values())
+        if proxy not in services:
+            if service_id != str(uuid.uuid4()):
+                if proxy.ice_isA('::IceFlix::Authenticator'):
+                    print(f'New AuthServer: {service_id}')
+                    self.authenticator_list.append(IceFlix.AuthenticatorPrx.uncheckedCast(proxy))
 
-        if proxy.ice_isA('::IceFlix::Authenticator'):
-            print(f'New AuthServer: {service_id}')
-            self.authenticatorList.append(IceFlix.AuthenticatorPrx.uncheckedCast(proxy))
+                if proxy.ice_isA('::IceFlix::MediaCatalog'):
+                    print(f'New CatalogServer: {service_id}')
+                    self.catalog_list.append(IceFlix.MediaCatalogPrx.uncheckedCast(proxy))
 
-        if proxy.ice_isA('::IceFlix::MediaCatalog'):
-            print(f'New CatalogServer: {service_id}')
-            self.catalogList.append(IceFlix.MediaCatalogPrx.uncheckedCast(proxy))
+                if proxy.ice_isA('::IceFlix::FileService'):
+                    print(f'New FileServer: {service_id}')
+                    self.file_list.append(IceFlix.FileServicePrx.uncheckedCast(proxy))
 
-        if proxy.ice_isA('::IceFlix::FileService'):
-            print(f'New FileServer: {service_id}')
-            self.fileList.append(IceFlix.FileServicePrx.uncheckedCast(proxy))
-
-        if proxy.ice_isA('::IceFlix::Main'):
-            print(f'New MainServer: {service_id}')
-            self.mains.update({service_id:proxy})
+                if proxy.ice_isA('::IceFlix::Main'):
+                    print(f'New MainServer: {service_id}')
+                    self.mains.update({service_id:proxy})
 
 class MainApp(Ice.Application):
     """Example Ice.Application for a Main service."""
@@ -128,8 +121,9 @@ class MainApp(Ice.Application):
         self.adapter = None
 
     def annoucement(self, servant, proxy):
+        """Annoucement method"""
         servant.announce(proxy, str(uuid.uuid4()))
-        timer = threading.Timer((10+random.randint(-2,2)), self.annoucement, (servant, proxy))
+        timer = threading.Timer(25, self.annoucement, (servant, proxy))
         timer.start()
 
     def run(self, args):
@@ -137,16 +131,13 @@ class MainApp(Ice.Application):
         logging.info("Running Main application")
         comm = self.communicator()
         self.adapter = comm.createObjectAdapter("MainAdapter")
-        self.proxy = self.adapter.addWithUUID(self.servant)
         self.adapter.activate()
+        self.proxy = self.adapter.addWithUUID(self.servant)
 
         self.servant.newService(self.proxy, str(uuid.uuid4()))
-
         self.timerr.start()
-
-        timer = threading.Timer(10, self.annoucement, (self.servant, self.proxy))
+        timer = threading.Timer(3, self.annoucement, (self.servant, self.proxy))
         timer.start()
-
         print("Main Server: " +str(self.proxy), flush=True)
 
         self.shutdownOnInterrupt()
